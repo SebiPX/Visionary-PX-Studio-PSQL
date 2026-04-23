063030ac1b99b62ab4b00143f46715f82efc8933
SebiPX <sebastian.geller@pixelschickeria.de>
2026-04-23 10:21:16 +0200

    fix: allow self-signed ssl for aiNewsDb

--- labs-api/src/aiNewsDb.ts
+++ labs-api/src/aiNewsDb.ts
@@ -2,6 +2,7 @@ import { Pool } from 'pg';
 
 const aiNewsPool = new Pool({
   connectionString: 'postgresql://ai_news_user:ai_news_pw_change_me@72.62.52.75:5432/ai_news?sslmode=require',
+  ssl: { rejectUnauthorized: false },
   max: 5,
   idleTimeoutMillis: 30000,
   connectionTimeoutMillis: 5000,
