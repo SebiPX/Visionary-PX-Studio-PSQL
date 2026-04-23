b667934f000c30e988aefbc0499aab4793595cd9
SebiPX <sebastian.geller@pixelschickeria.de>
2026-04-23 10:14:47 +0200

    fix: update db password

--- labs-api/src/aiNewsDb.ts
+++ labs-api/src/aiNewsDb.ts
@@ -1,7 +1,7 @@
 import { Pool } from 'pg';
 
 const aiNewsPool = new Pool({
-  connectionString: 'postgresql://ai_news_user:Unsere-Schickeria-2025@72.62.52.75:5432/ai_news?sslmode=require',
+  connectionString: 'postgresql://ai_news_user:ai_news_pw_change_me@72.62.52.75:5432/ai_news?sslmode=require',
   max: 5,
   idleTimeoutMillis: 30000,
   connectionTimeoutMillis: 5000,
