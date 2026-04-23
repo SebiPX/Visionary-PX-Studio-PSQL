932c06aa56eb2222c3f9a27df1615e48cd625529
SebiPX <sebastian.geller@pixelschickeria.de>
2026-04-23 10:23:35 +0200

    fix: remove sslmode parameter so rejectUnauthorized takes precedence

--- labs-api/src/aiNewsDb.ts
+++ labs-api/src/aiNewsDb.ts
@@ -1,7 +1,7 @@
 import { Pool } from 'pg';
 
 const aiNewsPool = new Pool({
-  connectionString: 'postgresql://ai_news_user:ai_news_pw_change_me@72.62.52.75:5432/ai_news?sslmode=require',
+  connectionString: 'postgresql://ai_news_user:ai_news_pw_change_me@72.62.52.75:5432/ai_news',
   ssl: { rejectUnauthorized: false },
   max: 5,
   idleTimeoutMillis: 30000,
